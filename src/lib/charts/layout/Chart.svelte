da2b59ae37a2144fe3c2fd28fc71cbe9fb028864
jordanSatsih <jordansalih@outlook.com>
2023-02-17 09:31:49 +0800

    Update for github pages deployment

--- src/lib/charts/layout/Chart.svelte
+++ src/lib/charts/layout/Chart.svelte
@@ -3,10 +3,11 @@
 
 	export let title: string;
 	export let query_link: string;
+	export let description: string;
 </script>
 
 <div class="border-2 border-black rounded-lg p-2 bg-white">
-	<ChartHeader {title} {query_link} />
+	<ChartHeader {title} {query_link} {description}/>
 	<div class="relative flex-auto">
 		<slot name="figure"><span>Figure</span></slot>
 	</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: dev ? '' : '/Web3-Analytics-Template'
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal'
 	}
 };
 
